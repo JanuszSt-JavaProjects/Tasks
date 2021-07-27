f533bc88c440a85c8bbd7319703cb17dfd6726bd
Janusz <mail.janusz@wp.pl>
2021-07-27 18:11:19 +0200

    19.2  Task

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,26 +1,43 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
-
-@RequestMapping("/v1/task")
 @RestController
+@RequestMapping("/v1/task")
+@RequiredArgsConstructor                                  // 4me comment: - zastępuje konstruktor WRAZ Z adnotacją.
 public class TaskController {
 
+    private final DbService service;
+    private final TaskMapper taskMapper;
+
+/*    @Autowired
+    public TaskController(DbService service, TaskMapper taskMapper) {
+        this.service = service;
+        this.taskMapper = taskMapper;
+    }*/
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        return new ArrayList<>();
+        List<Task> tasks = service.getAllTasks();
+        return taskMapper.mapToTaskDtoList(tasks);
+
     }
 
     @GetMapping(value = "getTask")
     public TaskDto getTask(Long taskId) {
-        return new TaskDto(1L, "test title", "test_content");
+
+            return taskMapper
+                    .mapToTaskDto(
+                            service.getTask(taskId).get()
+                    );
     }
 
     @DeleteMapping(value = "deleteTask")
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,36 @@
+package com.crud.tasks.service;
+
+
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor                          //4me comment: adnotacja zastępuje zakomentowany konstruktor.
+
+public class DbService {
+
+    private final TaskRepository repository;
+
+    /*    public DbService(TaskRepository repository) {
+            this.repository = repository;
+        }
+    */
+
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Optional<Task> getTask(Long id) {
+
+        return repository.findById(id);
+
+
+    }
+
+}
