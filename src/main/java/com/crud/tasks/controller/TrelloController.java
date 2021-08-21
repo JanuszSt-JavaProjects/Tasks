e1ecc63b1a21caf7193b771a43e50c53bbfb671d
Janusz <mail.janusz@wp.pl>
2021-08-21 18:00:32 +0200

    22.2 Task

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -19,10 +20,28 @@ public class TrelloController {
     @GetMapping("getTrelloBoards")
     public void getTrelloBoards() {
 
+
+        /* Zadanie ppkt -> .3   */
+
+/*
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
-        trelloBoards.forEach(trelloBoardDto -> {
-            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-        });
+        trelloBoards
+                .stream().filter(x -> x.getId() != null && x.getName() != null && x.getName().contains("Kodilla"))
+                .forEach(trelloBoardDto -> {
+                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                });
+
+*/
+
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream()
+                .filter(x -> Optional.ofNullable(x.getId()).isPresent())
+                .filter(x -> Optional.ofNullable(x.getName()).isPresent())   // jeśli z założenia wyszukujemy tylko "Kodillę" to tę linię można pominąć.
+                .filter(x -> x.getName().contains("Kodilla"))
+                .forEach(x -> System.out.println(x.getId().concat("   ").concat(x.getName())));
+
     }
 }
--- src/main/java/com/crud/tasks/terllo/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/terllo/client/TrelloClient.java
@@ -8,7 +8,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.URI;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 
 //    TrelloBoardDto[] boardsResponse = restTemplate.getForObject(          // wersja z konkatencją
@@ -27,31 +30,39 @@ import java.util.*;
 //    }
 
 
+@Component
+@RequiredArgsConstructor
+public class TrelloClient {
 
-    @Component
-    @RequiredArgsConstructor
-    public class TrelloClient {
+    private final RestTemplate restTemplate;
 
-        private final RestTemplate restTemplate;
-
-        @Value("${trello.api.endpoint.prod}")
-        private String trelloApiEndpoint;
-        @Value("${trello.app.key}")
-        private String trelloAppKey;
-        @Value("${trello.app.token}")
-        private String trelloToken;
+    @Value("${trello.api.endpoint.prod}")
+    private String trelloApiEndpoint;
+    @Value("${trello.app.key}")
+    private String trelloAppKey;
+    @Value("${trello.app.token}")
+    private String trelloToken;
+    @Value("${trello.app.username}")
+    private String trelloUsername;
 
 
-        public List<TrelloBoardDto> getTrelloBoards() {
-            URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/januszs2/boards")
-                    .queryParam("key", trelloAppKey)
-                    .queryParam("token", trelloToken)
-                    .queryParam("fields", "name,id")
-                    .build()
-                    .encode()
-                    .toUri();
+    public List<TrelloBoardDto> getTrelloBoards() {
 
-            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
+        return getTrelloURL();
+    }
+
+
+    private List<TrelloBoardDto> getTrelloURL() {
+
+        URI url = UriComponentsBuilder.fromHttpUrl(trelloApiEndpoint + "/members/" + trelloUsername + "/boards")
+                .queryParam("key", trelloAppKey)
+                .queryParam("token", trelloToken)
+                .queryParam("fields", "name,id")
+                .build()
+                .encode()
+                .toUri();
+
+        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(url, TrelloBoardDto[].class);
 
      /*
             if (boardsResponse != null) {
@@ -60,11 +71,9 @@ import java.util.*;
             return new ArrayList<>();
        */                                                               //To samo tyle, że jako Optional:
 
-            return Optional.ofNullable(boardsResponse)
-                    .map(Arrays::asList)
-                    .orElse(Collections.emptyList());
+        return Optional.ofNullable(boardsResponse)
+                .map(Arrays::asList)
+                .orElse(Collections.emptyList());
 
-        }
-
-
+    }
 }
